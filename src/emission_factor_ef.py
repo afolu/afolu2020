c15e86609e73c0ef36a94e3c880f580d3da0f984
aladinor <aladinor@unal.edu.co>
2020-05-17 11:45:38 -0500

    adding ym factor from table to emision factor

--- src/emission_factor_ef.py
+++ src/emission_factor_ef.py
@@ -1,16 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from gross_energy import GrossEnergy
+from db_utils import pg_connection
 
 
 class FactorEF(GrossEnergy):
-    def __init__(self, **kwargs):
+    def __init__(self, ym_id=1, **kwargs):
+        """
+        :param ym_id: indicie del factor ym
+        :param kwargs: parámetros para la caractrización del animal tipo
+        """
         super().__init__(**kwargs)
+        self.ym = ym_id
+
+    def get_from_ym_table(self):
+        """
+        Get data from ym table
+        :return: cp
+        """
+        conn = pg_connection()
+        with conn as connection:
+            query = """
+            SELECT  ym_eb
+            FROM ym
+            WHERE id_ym = '{0}'
+            """.format(self.ym)
+            cur = connection.cursor()
+            cur.execute(query)
+            res = cur.fetchall()
+            ym = res[0][0]
+        return ym
 
     def gbvap(self):
         """
-        3A2ai Ganado Bovino Vacas de Alta Producción
-        :return:
+        3A1ai Ganado Bovino Vacas de Alta Producción
+        :return: factor de emision para la categoria 3A2ai
         """
         assert 250 <= self.weight <= 700, "El peso vivo promedio de estas razas a nivel nacional puede variar desde " \
                                           "los 250 a 700 kg tanto promedio como adulto"
@@ -18,57 +42,90 @@ class FactorEF(GrossEnergy):
         assert 2440 <= self.milk <= 15250, "El rango de producción de leche puede ir de 2,440 a " \
                                            "15,250 litros por lactancia"
         assert 2.0 <= self.grease <= 6.0, "El contenido de grasa en la leche puede ir desde el 2.0 % hasta el 6.0%"
-
-        gbvap_en = self.maintenance() + self.activity() + self.breastfeeding() + self.pregnancy()
-        return gbvap_en
+        assert self.ca_id == 1, "La categoria de ganado bovino vacas de alta producción solo puede estar conformado " \
+                                "animales tipo Bos Taurus (1)"
+        gbvap_en = self.maintenance() + self.activity() + self.breastfeeding() + self.pregnancy() + self.work()
+        ym = self.get_from_ym_table()
+        ef = (gbvap_en * (ym / 100) * 365) / 55.65
+        return ef, gbvap_en
 
     def gbvbp(self):
         """
-        3A2aii Ganado Bovino Vacas de Baja Producción
-        :return:
+        3A1aii Ganado Bovino Vacas de Baja Producción
+        :return: factor de emisión para la categoria 3A2aii
         """
-        gbvbp_en = self.maintenance() + self.activity() + self.breastfeeding() + self.pregnancy()
-        return gbvbp_en
+        gbvbp_en = self.maintenance() + self.activity() + self.breastfeeding() + self.pregnancy() + self.work()
+        ym = self.get_from_ym_table()
+        ef = (gbvbp_en * (ym / 100) * 365) / 55.65
+        return ef, gbvbp_en
 
     def gbvpc(self):
         """
-        3A2aiii Ganado Bovino Vacas para Producción de Carne
+        3A1aiii Ganado Bovino Vacas para Producción de Carne
         :return:
         """
-        gbvpc_en = self.maintenance() + self.activity() + self.breastfeeding() + self.pregnancy()
-        return gbvpc_en
+        gbvpc_en = self.maintenance() + self.activity() + self.breastfeeding() + self.pregnancy() + self.work()
+        ym = self.get_from_ym_table()
+        ef = (gbvpc_en * (ym / 100) * 365) / 55.65
+        return ef, gbvpc_en
 
     def gbtpfr(self):
         """
-        3A2aiv Ganado Bovino Toros utilizados con fines reproductivos
+        3A1aiv Ganado Bovino Toros utilizados con fines reproductivos
         :return:
         """
-        gbtpfr_en = self.maintenance() + self.activity()
-        return gbtpfr_en
+        gbtfr_en = self.maintenance() + self.activity() + self.work()
+        ym = self.get_from_ym_table()
+        ef = (gbtfr_en * (ym / 100) * 365) / 55.65
+        return ef, gbtfr_en
 
     def gbtpd(self):
         """
-        3A2av Ganado Bovino Terneros pre-destetos
+        3A1av Ganado Bovino Terneros pre-destetos
         :return:
         """
-        gbtpd_en = self.maintenance() + self.activity() + self.grow() + self.milk()
-        return gbtpd_en
+        gbtpd_en = self.maintenance() + self.activity() + self.grow() + self.work() - self.milk()
+        ym = self.get_from_ym_table()
+        ef = (gbtpd_en * (ym / 100) * 273.75) / 55.65
+        return ef, gbtpd_en
 
     def gbtr(self):
         """
-        3A2avi Ganado Bovino Terneras de remplazo
+        3A1avi Ganado Bovino Terneras de remplazo
         :return:
         """
-        gbtr_en = self.maintenance() + self.activity() + self.grow()
-        return gbtr_en
+        gbtr_en = self.maintenance() + self.activity() + self.grow() + self.work()
+        ym = self.get_from_ym_table()
+        ef = (gbtr_en * (ym / 100) * 365) / 55.65
+        return ef, gbtr_en
 
     def gbge(self):
         """
-        3A2avii Ganado Bovino Ganado de engorde
+        3A1avii Ganado Bovino Ganado de engorde
         :return:
         """
-        gbge_en = self.maintenance() + self.activity() + self.grow()
-        return gbge_en
+        gbge_en = self.maintenance() + self.activity() + self.grow() + self.work()
+        ym = self.get_from_ym_table()
+        ef = (gbge_en * (ym / 100) * 365) / 55.65
+        return ef, gbge_en
+
+    def cms(self, ge_t):
+        """
+        Consumo de materia seca (calculado a través del consumo de energía)
+        :param ge_t: total gross energy
+        :return: cms (kg dia-1)
+        """
+        c_ms = ge_t / self.d_ep() 
+        return c_ms
+
+    def cms_pv(self, c_ms):
+        """
+        Consumo de materia seca como porcentaje de peso vivo
+        :param c_ms: Consumo de materia seca
+        :return: cmspv
+        """
+        cmspv = c_ms / self.weight
+        return cmspv
 
 
 def main():
