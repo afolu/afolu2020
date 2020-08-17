e47371f0ec62c04b2a296ff43c20479df4046410
aladinor <aladinor@unal.edu.co>
2020-08-17 17:18:57 -0500

    refactoring code for forest plant calculation

--- src/land_use/forest_plant.py
+++ src/land_use/forest_plant.py
@@ -3,7 +3,6 @@
 import sys
 import os
 import pandas as pd
-from datetime import datetime
 from numpy import arange
 
 sys.path.insert(0, f"{os.path.abspath(os.path.join(os.path.abspath(__file__), '../../../'))}")
@@ -45,21 +44,23 @@ def get_data(esp=None, sub_reg=None, z_upra=None, dpto=None, muni=None):
     return df
 
 
-def years_calc(ano_est, year_min, year_max):
-    assert year_max > year_min, 'El rango de consulta debe ser año inferior y año superior. Por favor verificar años ' \
-                                'consultados'
+def years_calc(ano_est, year):
+    year_min, year_max = year[0], year[-1]
+    if (len(year) == 1) and (year_max < ano_est):
+        return 0
+    elif (len(year) == 1) and (year_max > ano_est):
+        return 1
     if (year_min <= ano_est) and (year_max > ano_est):
-        cant = year_max - ano_est
+        return year_max - ano_est
     elif year_min > ano_est:
-        cant = year_max - year_min
+        return year_max - year_min
     elif year_max == ano_est:
-        cant = 1
-    elif year_max < ano_est:
-        cant = 0
-    return cant
+        return 1
 
 
 def turns(ano_est, turno, year_min, year_max):
+    assert year_max >= year_min, 'El rango de consulta debe ser año inferior y año superior. Por favor verificar ' \
+                                 'años consultados'
     turnos = [int(ano_est + turno * i) for i in range(1000)]
     turnos.pop(0)
     if year_max == year_min:
@@ -71,34 +72,31 @@ def turns(ano_est, turno, year_min, year_max):
 
 
 def gross_abs(year, esp=None, sub_reg=None, z_upra=None, dpto=None, muni=None):
-    df = get_data(esp, sub_reg=sub_reg, z_upra=z_upra, dpto=dpto, muni=muni)
+    df = get_data(esp=esp, sub_reg=sub_reg, z_upra=z_upra, dpto=dpto, muni=muni)
     df['abs_BA_year'] = df['factor_cap_carb_ba'] * df['hectareas']
     df['abs_BT_year'] = df['factor_cap_carb_bt'] * df['hectareas']
-    if len(year) == 1:
-        df['abs_BA_tot'] = df['abs_BA_year']
-        df['abs_BT_tot'] = df['abs_BT_year']
-    else:
-        df['cant'] = df['ano_establecimiento'].apply(lambda x: years_calc(x, year_min=year[0], year_max=year[-1]))
-        df['abs_BA_tot'] = df['abs_BA_year'] * df['cant']
-        df['abs_BT_tot'] = df['abs_BT_year'] * df['cant']
-    # df.to_csv('/home/alfonso/Documents/afolu/results/abs.csv')
-    print(df.head)
-
-
-def gross_emi(year, esp=None, sub_reg=None, z_upra=None, dpto=None, muni=None):
-    df = get_data(esp=esp, sub_reg=sub_reg, z_upra=z_upra, dpto=dpto, muni=muni)
+    df['cant'] = df['ano_establecimiento'].apply(lambda x: years_calc(x, year=year))
+    df['accum'] = - df['ano_establecimiento'] + year[0]
+    df['accum'].loc[df['accum'] < 0] = 0
+    df['abs_BA_tot'] = - df['abs_BA_year'] * df['cant']
+    df['abs_BT_tot'] = - df['abs_BT_year'] * df['cant']
+    df['abs_BA_accum'] = - df['abs_BA_year'] * df['accum']
+    df['abs_BT_accum'] = - df['abs_BT_year'] * df['accum']
     df['abs_BA_year'] = df['factor_cap_carb_ba'] * df['hectareas']
     df['abs_BT_year'] = df['factor_cap_carb_bt'] * df['hectareas']
     df['turnos'] = df.apply(lambda x: turns(x['ano_establecimiento'], x['turno'], year_max=year[-1], year_min=year[0]),
                             axis=1)
     df['ems_BA_tot'] = df['abs_BA_year'] * df['turnos'] * df['turno']
     df['ems_BT_tot'] = df['abs_BT_year'] * df['turnos'] * df['turno']
-    df.head()
+    df['ems_BA_neta'] = df['abs_BA_tot'] + df['ems_BA_tot']
+    df['ems_BT_neta'] = df['abs_BT_tot'] + df['ems_BT_tot']
+    df = df.fillna(0)
+    df.to_sql('land_use_res', con=pg_connection_str(), index=False, if_exists='replace', method="multi", chunksize=5000)
+    print('Done')
 
 
 def main():
-    gross_abs(year=[2000], esp=[69, 158], sub_reg=[1, 2], z_upra=[1], dpto=[23, 17, 15])
-    gross_emi(year=[1980, 2011], esp=[69, 158], sub_reg=[1, 2], z_upra=[1], dpto=[23, 17, 15])
+    gross_abs(year=[2000, 2011], esp=[69, 158], sub_reg=[1, 2], z_upra=[1], dpto=[23, 17, 15])
 
 
 if __name__ == '__main__':
